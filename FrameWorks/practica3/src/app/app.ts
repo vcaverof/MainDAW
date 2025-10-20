35e63c07cc455277ee8568dbf2664ccdb77cd173
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-10-20 13:55:22 +0200

    FRAMEWORKS - Practica 3 terminada

--- FrameWorks/practica3/src/app/app.ts
+++ FrameWorks/practica3/src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, QueryList, signal, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SensorTemperatura } from './sensor-temperatura/sensor-temperatura';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SensorTemperatura],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('practica3');
+  sensores = [
+    { nombre: 'Sensor 1', rango: { min: 0, max: 50 } },
+    { nombre: 'Sensor 2', rango: { min: 10, max: 60 } },
+    { nombre: 'Sensor 3', rango: { min: 5, max: 40 } },
+  ];
+
+  @ViewChildren(SensorTemperatura) sensoresRef!: QueryList<SensorTemperatura>;
+
+  fijarTemperaturas() {
+    this.sensoresRef.forEach((sensor, index) => {
+      const valor = Math.floor(Math.random() * 100);
+      sensor.fijar(valor);
+    });
+  }
+
+  reiniciarTemperaturas() {
+    this.sensoresRef.forEach(sensor => sensor.reset());
+  }
+
+  manejarAlerta(mensaje: string) {
+    alert(mensaje);
+  }
 }
