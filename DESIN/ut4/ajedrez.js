419ab0cce2842fd36b9573356f4fc8f0419c0ba9
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-11-02 13:24:29 +0100

    CLIENTE - Terminado el ejercicio del ajedrez

--- DESIN/ut4/ajedrez.js
+++ DESIN/ut4/ajedrez.js
@@ -9,40 +9,59 @@ for (let i = 0; i < filas; i++) {
     }
 }
 
-function Figura() {
+function Figura(tipo) {
+
+    this.tipo = tipo;
     this.x = 0;
     this.y = 0;
-    // moverA(x, y) = function () {
 
-    // }
+    //IMPORTANTE AÑADIR EL THIS
+    this.moverA = function(x, y) {
+        //Primero vaciar la posicion anterior
+        tablero[this.y][this.x] = " ";
+
+        //Seleccionar las coordenadas nuevas
+        this.x = x;
+        this.y = y;
+
+        //Colocar la figura en su nueva posicion
+        tablero[y][x] = this.tipo
+    }
+
 }
 
 let figura = new Figura();
 
-let rey = { tipo: 'R', ...figura };
-let reina = { tipo: 'Q', ...figura };
-let torre = { tipo: 'T', ...figura };
-let alfil = { tipo: 'A', ...figura };
-let caballo = { tipo: 'C', ...figura };
-let peon = { tipo: 'P', ...figura };
+let rey = new Figura('R');
+let reina = new Figura('Q');
+let torre = new Figura('T');
+let alfil = new Figura('A');
+let caballo = new Figura('C');
+let peon = new Figura('P');
 
 let figuras = [rey, reina, torre, torre, alfil, alfil, caballo, caballo, peon, peon, peon, peon, peon, peon, peon, peon];
 
 //Coloca todas las piezas en zonas aleatorias del tablero
 function colocarPieza(figura, tablero) {
-    let filaRandom = () => Math.floor(Math.random() * 7.99);
-    let columnaRandom = () => Math.floor(Math.random() * 7.99);
+    let fila, columna;
 
-    fila = filaRandom();
-    columna = columnaRandom();
+    //Comprobar si la celda está ocupada por otra pieza
+    do {
+        fila = Math.floor(Math.random() * filas);
+        columna = Math.floor(Math.random() * columnas);
+    } while (tablero[fila][columna] !== " ");
 
     tablero[fila][columna] = figura.tipo;
-
 }
+
 
 for (let i = 0; i < figuras.length; i++) {
     colocarPieza(figuras[i], tablero);
 }
+
+console.table(tablero);
+
+rey.moverA(4,7);
 
 console.table(tablero);
 
