3ce4a674f2721bd09fb79a4d610cab68c0d8884b
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-11-06 10:55:10 +0100

    DWES - Ejercicio propio barcos

---
+++ DWES/juegoBarcos/index.php
@@ -0,0 +1,16 @@
+<body>
+    <form method="post">
+        <label>Introduce el número máximo de intentos</label>
+        <input type="number" name="intentos" id="intentos" value="40" min="1"><br><br>
+
+        <label for="modo">Modo de generación de barcos</label>
+        <select name="modo" id="modo">
+            <option>Generar aleatoriamente</option>
+            <option>Generar mediante fichero</option>
+        </select>
+
+
+
+
+    </form>
+</body>
