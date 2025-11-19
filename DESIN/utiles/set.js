c3c5e4301250405c32bad6f6ac21324c1c6e6ffb
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-11-19 11:16:24 +0100

    CLIENTE - EXAMEN

--- DESIN/utiles/set.js
+++ DESIN/utiles/set.js
@@ -4,6 +4,7 @@ const conjunto = new Set([1, 2, 3, 3, 4]);
 conjunto.add(5);        // Añadir → {1,2,3,4,5}
 conjunto.has(3);        // ¿Existe? → true
 conjunto.delete(2);     // Eliminar → {1,3,4,5}
+conjunto.values()       //Objeto iterable con cada uno de los valores del conjunto
 conjunto.size;          // Tamaño → 4
 
 // Recorrer
