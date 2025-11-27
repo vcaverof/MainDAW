3def399aa8ab9ba5d6f3136d086589f6e8ddf419
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-11-27 15:00:22 +0100

    FRAMEWORKS - Practica 5 empezada

--- FrameWorks/practica5/src/app/app.ts
+++ FrameWorks/practica5/src/app/app.ts
@@ -1,12 +1,60 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('practica5');
+  nombre = "";
+  email = "";
+  biografia = "";
+  genero = "";
+  intereses = "";
+  nivelAngular = "";
+
+  formUsuario = new FormGroup({
+    nombre: new FormControl(''),
+    email: new FormControl(''),
+    biografia: new FormControl(''),
+    genero: new FormControl('femenino'),
+    preferencias: new FormGroup({
+      intereses: new FormControl('false'),
+      nivelAngular: new FormControl('Basico'),
+    })
+  });
+
+  submit() {
+    if (this.formUsuario.value.nombre) {
+      this.nombre = this.formUsuario.value.nombre;
+    }
+
+    if (this.formUsuario.value.email) {
+      this.email = this.formUsuario.value.email;
+    }
+
+    if (this.formUsuario.value.biografia) {
+      this.biografia = this.formUsuario.value.biografia;
+    }
+
+    if (this.formUsuario.value.genero == "masculino") {
+      this.genero = "Masculino";
+    } else if (this.formUsuario.value.genero == "femenino") {
+      this.genero = "Femenino";
+    } else {
+      this.genero = "Otro";
+    }
+
+    if (this.formUsuario.value.preferencias?.intereses) {
+      this.intereses = this.formUsuario.value.preferencias.intereses; //IMRIME TRUE, NO EL VALOR DE LOS CHECKBOX
+    }
+
+    if(this.formUsuario.value.preferencias?.nivelAngular) {
+      this.nivelAngular = this.formUsuario.value.preferencias.nivelAngular.toString();
+    }
+
+  }
 }
