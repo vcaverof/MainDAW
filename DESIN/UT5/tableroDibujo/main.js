83ab4ee2a892f6eb88a87bd167b8e6bef00b7c20
vcaverof <victor.cavfer@iesjulianmarias.es>
2025-12-10 20:54:00 +0100

    CLIENTE - Ejercicio pintar terminado

--- DESIN/UT5/tableroDibujo/main.js
+++ DESIN/UT5/tableroDibujo/main.js
@@ -1,3 +1,22 @@
+
+let colorSeleccionado = "color1"; //Colos seleccionado por defecto
+let pincelActivo = false;
+
+function activarPincel(event) {
+    pincelActivo = !pincelActivo;
+    document.getElementById("pincel").textContent = pincelActivo ? "PINCEL ACTIVO" : "PINCEL DESACTIVADO";
+
+    if (pincelActivo) {
+        event.target.className = colorSeleccionado;
+    }
+}
+
+function pintar(event) {
+    if (pincelActivo) {
+        event.target.className = colorSeleccionado;
+    }
+}
+
 //Dibujar tablero y añadirlo al div "zonadibujo"
 const tabla = document.createElement("table");
 tabla.className = "tablerodibujo";
@@ -6,20 +25,30 @@ for (let i = 0; i < 30; i++) {
     const fila = document.createElement("tr");
     for (let j = 0; j < 30; j++) {
         const td = document.createElement("td");
+        td.style.border = "solid 1px black";
+        
+        //Eventos
+        td.addEventListener('click', activarPincel);
+        td.addEventListener('mouseover', pintar);
+
         fila.appendChild(td);
     }
     tabla.appendChild(fila);
 }
 document.getElementById("zonadibujo").appendChild(tabla);
 
-//Seleccionar color y pincel
+//Seleccionar color
 document.getElementById("paleta").addEventListener('click', seleccionarColor);
 
 function seleccionarColor(event) {
-    const evento = document.getElementById(event.target.id);
+    if (event.target.tagName === "TD") {
+        const colores = document.querySelectorAll("#paleta td");
+        colores.forEach(color => color.classList.remove("seleccionado"));
 
-    //Cambiar la clase al color seleccionado
-    if (evento.classList.length < 2) {
-        evento.className = evento.className + " seleccionado";
+        event.target.classList.add("seleccionado");
+        colorSeleccionado = event.target.classList[0];
     }
 }
+
+
+
