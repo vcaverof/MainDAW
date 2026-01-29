adca64e13ea80e17b5df386d6f231e8867b013f8
vcaverof <victor.cavfer@iesjulianmarias.es>
2026-01-29 10:00:25 +0100

    CLIENTE - Ejercicios de AJAX y JSON

--- DESIN/UT7/EjemplosPHP/recibirfichero.php
+++ DESIN/UT7/EjemplosPHP/recibirfichero.php
@@ -1,6 +1,6 @@
 <?php
 
-$fichero_subido = 'C:\\Users\\victor.cavfer\\Desktop\\MainDAW-1\\DESIN\\UT7\\EjemplosPHP' . basename($_FILES['mifichero']['name']);
+$fichero_subido = '.\\' . basename($_FILES['mifichero']['name']);
 if (move_uploaded_file($_FILES['mifichero']['tmp_name'], $fichero_subido)) {
     echo basename($fichero_subido);
 } else {
