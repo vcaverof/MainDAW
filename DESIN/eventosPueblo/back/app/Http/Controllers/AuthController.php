a5d479c4b74b5bf459fbd6e1b2454ed888f1a2af
vcaverof <victor.cavfer@iesjulianmarias.es>
2026-02-15 18:06:54 +0100

    CLIENTE - Sitio web completo, funcionalidades añadidas al front

--- DESIN/eventosPueblo/back/app/Http/Controllers/AuthController.php
+++ DESIN/eventosPueblo/back/app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Login correcto',
             'token' => $token,
-            'username' => $user
+            'user' => $user
         ]);
     }
 
